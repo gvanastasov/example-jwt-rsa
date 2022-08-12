9b0af83b0c323be06af3a0dcd3f6db208e559357
Anastasov <14071890+gvanastasov@users.noreply.github.com>
2022-08-12 16:04:54 +0200

    Add jwks endpoint

---
+++ src/auth/controllers/jwkController.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const rsaPemToJwk = require('rsa-pem-to-jwk')
+
+exports.index = function (_request, response) {
+  // todo: use cache instead
+
+  // todo: use async instead
+  const buffer = fs.readFileSync('./certs/private.pem')
+  const secret = buffer.toString().replace(/\r\n/g, '\n')
+  const jwk = rsaPemToJwk(secret, { use: 'sig' }, 'public')
+  console.log(secret)
+  console.log(jwk)
+  response.send({
+    keys: [
+      jwk
+    ]
+  })
+}
--- src/auth/routes/index.js
+++ src/auth/routes/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const loginController = require('../controllers/loginController')
+const jwkController = require('../controllers/jwkController')
 
 router.get('/', loginController.index)
+
+router.get('/.well-known/jwks', jwkController.index)
 
 module.exports = router
